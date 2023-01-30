c2b1103cad0a83e1f766d08cf0b5545e5bedd376
zq <2603682659@qq.com>
2023-01-30 12:21:26 +0800

    fix

--- pages/api/homes/[id]/favorite.js
+++ pages/api/homes/[id]/favorite.js
@@ -1,5 +1,7 @@
-import { prisma } from "@/lib/prisma";
 import { getSession } from "next-auth/react";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
