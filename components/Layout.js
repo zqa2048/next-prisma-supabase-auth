7b274b081fc56789e8b52b1a43cbc8829c2ff24d
zq <2603682659@qq.com>
2023-02-01 13:39:58 +0800

    fix:build

--- components/Layout.js
+++ components/Layout.js
@@ -192,13 +192,9 @@ const Layout = ({ children = null }) => {
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
-                  class="inline-block w-4 h-4 -mt-1 text-red-600 animate-pulse"
+                  className="inline-block w-4 h-4 -mt-1 text-red-600 animate-pulse"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                    clip-rule="evenodd"
-                  ></path>
+                  <path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"></path>
                 </svg>
                 提供技术驱动 - 2023
               </a>
